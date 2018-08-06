a44dac163e6b482761fbabe42386ddadb533e2c5
Sergei Glushchenko <gl.sergei@gmail.com>
2018-08-07 03:48:53 +0700

    Fix bug1130627 test case

--- storage/innobase/xtrabackup/test/t/bug1130627.sh
+++ storage/innobase/xtrabackup/test/t/bug1130627.sh
@@ -19,55 +19,40 @@ CREATE TABLE t_innodb (
 ) ENGINE=InnoDB
 /*!50100 PARTITION BY HASH (i)
 PARTITIONS 10 */;
-
-CREATE TABLE t_myisam (
-  i INT NOT NULL AUTO_INCREMENT,
-  c CHAR(10) DEFAULT NULL,
-  PRIMARY KEY (i)
-) ENGINE=MyISAM
-/*!50100 PARTITION BY HASH (i)
-PARTITIONS 10 */;
 EOF
 
 force_checkpoint
 
-# Test that specifying partitions with --include works
-xtrabackup --backup --include='^test.*#P#p5' --target-dir=$topdir/backup
+# Test that specifying partitions with --tables works
+xtrabackup --backup --tables='^test.*#P#p5' --target-dir=$topdir/backup
 xtrabackup --prepare --target-dir=$topdir/backup
 
 file_cnt=`ls $topdir/backup/test | wc -l`
 
-test "$file_cnt" -eq 3
+test "$file_cnt" -eq 1
 
 test -f $topdir/backup/test/t_innodb#P#p5.ibd
-test -f $topdir/backup/test/t_myisam#P#p5.MYD
-test -f $topdir/backup/test/t_myisam#P#p5.MYI
 
 rm -rf $topdir/backup
 
 # Test that specifying partitions with --databases works
 
 xtrabackup --backup \
-    --databases='test.t_myisam#P#p5 test.t_innodb#P#p5' --target-dir=$topdir/backup
+    --databases='test.t_innodb#P#p5' --target-dir=$topdir/backup
 xtrabackup --prepare --target-dir=$topdir/backup
 
-test "$file_cnt" -eq 3
+test "$file_cnt" -eq 1
 test -f $topdir/backup/test/t_innodb#P#p5.ibd
-test -f $topdir/backup/test/t_myisam#P#p5.MYD
-test -f $topdir/backup/test/t_myisam#P#p5.MYI
 
 rm -rf $topdir/backup
 
 # Test that specifying partitions with --tables-file works
 cat >$topdir/tables_file <<EOF
-test.t_myisam#P#p5
 test.t_innodb#P#p5
 EOF
 xtrabackup --backup --tables-file=$topdir/tables_file --target-dir=$topdir/backup
-xtrabackup --preapre --target-dir=$topdir/backup
+xtrabackup --prepare --target-dir=$topdir/backup
 
-test "$file_cnt" -eq 3
-test -f $topdir/backup/test/t_myisam#P#p5.MYD
-test -f $topdir/backup/test/t_myisam#P#p5.MYI
+test "$file_cnt" -eq 1
 
 rm -rf $topdir/backup
